942b6dcd61b0f016fe110420234bcc6f40fcf9ec
jaiparmani <58816678+jaiparmani@users.noreply.github.com>
2021-07-08 20:31:48 +0530

    Delete MockProject-master directory

--- MockProject-master/Exceptions/admin.py
+++
@@ -1,6 +0,0 @@
-from Exceptions.models import ExceptionType
-from django.contrib import admin
-
-# Register your models here.
-from .models import ExceptionType
-admin.site.register(ExceptionType)
--- MockProject-master/Exceptions/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExceptionsConfig(AppConfig):
-    name = 'Exceptions'
